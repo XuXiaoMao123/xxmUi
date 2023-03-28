ad563ba16e1198b64898b3d097deee85328f4240
xiaomao.xu <xiaomao.xu@faw-vw.com>
2023-03-28 13:37:05 +0800

    feat: save

---
+++ components/lib/card/src/main.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mao-card" :style="width ? {width: width + 'px'} : {}">
+    <div class="mao-card-img" :style="imgHeight ? {height: imgHeight + 'px'} : {}">
+        <img :src="imgSrc" alt="img" />
+    </div>
+    <div v-if="summary" class="mao-card-summary">
+        {{ summary }}
+    </div>
+    <div v-else class="mao-card-summary">
+        <slot></slot>
+    </div>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'mao-card',
+    props: {
+        width: {//卡片宽度
+            type: Number,
+            default: 0,
+        },
+        imgSrc: {//卡片图片资源地址
+            type: String,
+            default: ''
+        },
+        imgHeight: {//卡片图片高度
+            type: Number,
+            default: 0
+        },
+        summary: {//卡片的概要
+            type: String,
+            default: '',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ examples/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <mao-demo />
+    <mao-card
+      imgSrc="/img/1.jpg"
+      summary="3月2日 “捷达之美”员工家属情感体验活动"
+    />
+    <br>
+    <mao-card
+      imgSrc="/img/2.jpg"
+      summary="3月8日 华北区客户情感体验活动"
+    >
+      <template v-slot:footer>
+        <div class="footer">
+          <div class="level">中级 528人报名</div>
+          <div class="price">￥299.00</div>
+        </div>
+      </template>
+    </mao-card>
+    <br>
+    <mao-card 
+      imgSrc="/img/3.jpg"
+      :width="370"
+      :imgHeight="90"
+    >
+      3月10日 华北区客户情感体验活动
+      <template v-slot:footer>
+        <div class="footer-spring">
+          <div class="level">4步骤 • 6门课</div>
+          <div class="level">10965收藏</div>
+        </div>
+      </template>
+    </mao-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.footer {
+  padding: 0 8px;
+  font-size: 1vw;
+  text-align: left;
+}
+.level {
+  color: #9199a1;
+  margin-bottom: 8px;
+}
+.price {
+  color: #F01414;
+}
+.footer-spring {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 8px;
+  font-size: 1vw;
+}
+</style>
